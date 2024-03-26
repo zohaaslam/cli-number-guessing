9a5c75f97b204e8a0756167a171bbc84e14b4e82
Zoha Aslam <160209815+zohaaslam@users.noreply.github.com>
2024-03-26 21:05:42 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquirer.prompt([
+    
+{
+message: "Please guessed a number between 1-6",
+type: "number",
+name: "userGuessedNumber",
+
+
+
+}]);
+
+if(answers.userGuessedNumber === randomNumber){
+
+    console.log("Congratulations! you guessed  right number.");
+}else{
+
+    console.log("You guessed  wrong number,Please Try Again");
+};
